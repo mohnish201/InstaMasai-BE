0d1ba05311ffa30cd80b9b1a1009251b132e639b
Mohnish Vishwakarma <mohnish201@gmail.com>
2023-08-21 14:12:00 +0530

    Additonal functionality added

--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -4,68 +4,89 @@ const { PostModel } = require("../model/PostModel");
 
 const PostRoute = express.Router();
 
-PostRoute.post("/add", auth , async(req, res)=> {
+PostRoute.post("/add", auth, async (req, res) => {
+  console.log(req.body);
 
-    console.log(req.body)
-    
-     try {
-        const NewPost = new PostModel(req.body);
-        await NewPost.save();
-        res.json("New Post has been Created Successfully")
-     } catch (error) {
-        res.send(error)
-     }
-})
+  try {
+    const NewPost = new PostModel(req.body);
+    await NewPost.save();
+    res.json("New Post has been Created Successfully");
+  } catch (error) {
+    res.send(error);
+  }
+});
 
-PostRoute.get("/", auth, async(req, res)=> {
-    
-try {
-    const Posts = await PostModel.find({userId: req.body.userId});
-    res.json(Posts)
-} catch (error) {
-    res.json(error)
-}
-})
+PostRoute.get("/", auth, async (req, res) => {
+  const {min, max, limit, page } = req.query;
+  let query = {};
+  if (min && max) {
+    query.$and = [
+      { no_of_comments: {$lte: max } },
+      { no_of_comments: {$gte: min } },
+    ];
+  }
 
-PostRoute.patch("/update/:id",auth,  async(req, res)=> {
-    const {id} = req.params;
+  const toSkip = (page - 1) * limit;
 
-    const posts = await PostModel.findOne({_id:id, userId:req.body.userId})
+  try {
+    const Posts = await PostModel.find({$and:[{userId: req.body.userId},query]}).skip(toSkip).limit(limi)
+      
+    res.json(Posts);
+  } catch (error) {
+    res.json(error);
+  }
+});
 
+
+PostRoute.get("/top", auth, async (req, res) => {
+
+   
+
+  
     try {
-        if(posts){
-            await PostModel.findByIdAndUpdate({_id:id}, req.body);
-            res.json("Post Updated")
-        }
-        else{
-            res.json("You are not authorized")
-        }
-       
+      const Posts = await PostModel.aggregate([{userId: req.body.userId}, {$sort: {no_of_comments:-1}}, {$limit: 3 }])
+        
+      res.json(Posts);
     } catch (error) {
-        res.json(error)
+      res.json(error);
     }
-})
+  });
+  
 
-PostRoute.delete("/delete/:id",auth,  async(req, res)=> {
-    const {id} = req.params;
+PostRoute.patch("/update/:id", auth, async (req, res) => {
+  const { id } = req.params;
 
-    const posts = await PostModel.findOne({_id:id, userId:req.body.userId})
+  const posts = await PostModel.findOne({ _id: id, userId: req.body.userId });
 
-    try {
-        if(posts){
-            await PostModel.findByIdAndDelete({_id:id});
-            res.json("Post Deleted")
-        }
-        else{
-            res.json("You are not authorized")
-        }
-       
-    } catch (error) {
-        res.json(error)
+  try {
+    if (posts) {
+      await PostModel.findByIdAndUpdate({ _id: id }, req.body);
+      res.json("Post Updated");
+    } else {
+      res.json("You are not authorized");
     }
-})
+  } catch (error) {
+    res.json(error);
+  }
+});
 
+PostRoute.delete("/delete/:id", auth, async (req, res) => {
+  const { id } = req.params;
 
-module.exports={
-    PostRoute
-}
+  const posts = await PostModel.findOne({ _id: id, userId: req.body.userId });
+
+  try {
+    if (posts) {
+      await PostModel.findByIdAndDelete({ _id: id });
+      res.json("Post Deleted");
+    } else {
+      res.json("You are not authorized");
+    }
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+module.exports = {
+  PostRoute,
+};
